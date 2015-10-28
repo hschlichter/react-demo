7182aee19569e0dc0418084706c3b82b9968a3fa
hschlichter <henrik.schlichter@gmail.com>
2015-10-28 09:40:20 +0100

    Removed non-working error handlers.

--- index.js
+++ index.js
@@ -30,28 +30,4 @@ app.use((req, res, next) => {
 	next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-//if (env === 'development') {
-//	app.use((err, req, res, next) => {
-//		res.status(err.status || 500);
-//		res.render('error', {
-//		message: err.message,
-//		error: err
-//		});
-//	});
-//}
-
-// production error handler
-// no stacktraces leaked to user
-//app.use((err, req, res, next) => {
-//	res.status(err.status || 500);
-//	res.render('error', {
-//	message: err.message,
-//		error: {}
-//	});
-//});
-
 module.exports = app;
