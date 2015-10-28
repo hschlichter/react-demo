a532307e50be656b4d38ff04dc1bf37c0896388a
hschlichter <henrik.schlichter@gmail.com>
2015-10-28 09:40:56 +0100

    Server-side fetching of state data.

--- pages/hackernews/index.js
+++ pages/hackernews/index.js
@@ -4,29 +4,39 @@ import ReactDOM from 'react-dom/server';
 import { Provider } from 'react-redux';
 import configureStore from '../../store/configurestore';
 import HackerNews from './components/hackernews';
+import { receive } from '../../actions/hackernews';
+import superagent from 'superagent';
 
 let router = express.Router();
 
 router.get('/hackernews', function(req, res, next) {
 	const store = configureStore();
 
-	let markup = ReactDOM.renderToString(
-		<Provider store={store}>
-			<HackerNews />
-		</Provider>
-	);
+	superagent
+		.get('https://www.kimonolabs.com/api/c30pdwri')
+		.end((err, res2) => {
+			if (res2.ok) {
+				store.dispatch(receive(res2.body.results.collection1));
+			}
 
-	let context = {
-		params: req.params,
-		state: store.getState()
-	};
+			let markup = ReactDOM.renderToString(
+				<Provider store={store}>
+					<HackerNews />
+				</Provider>
+			);
 
-	res.render('hackernews/views/main', {
-		js: '/js/hackernews.bundle.js',
-		context: JSON.stringify(context),
-		title: 'Express',
-		markup: markup
-	});
+			let context = {
+				params: req.params,
+				state: store.getState()
+			};
+
+			res.render('hackernews/views/main', {
+				js: '/js/hackernews.bundle.js',
+				context: JSON.stringify(context),
+				title: 'Express',
+				markup: markup
+			});
+		});
 });
 
 module.exports = router;
